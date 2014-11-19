756effbadd6ca4fe7eb559f1dcb5456a72923d7d
tomrcroft <tomrcroft@yahoo.com>
2014-11-19 03:38:11 -0800

    added basic register and login info
    new sql file to add necessary table

---
+++ loginFile.php
@@ -0,0 +1,28 @@
+<?php
+	include 'DBconstants.php';
+
+	$username = $_POST['username'];
+	$pword = $_POST['password'];
+  
+$con = mysqli_connect(SERVER, USERNAME, PASSWORD, DATABASENAME);
+
+		$sql = "SELECT *
+				FROM userinfo
+				WHERE username='$username';";
+
+	    $result = mysqli_query($con, $sql);
+		
+		if (mysqli_num_rows($result) == 0)
+				header('Location: loginPage.php');	
+		else
+		{
+			$resultArray = mysqli_fetch_array($result, MYSQLI_NUM);
+			
+			if($resultArray[1] == $pword)
+				header('Location: index.php');
+			else
+				header('Location: loginPage.php');
+		}
+		
+
+?>
