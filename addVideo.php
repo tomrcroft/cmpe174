55dadb61e5cf9db0a8d7a5883c913f193f47a013
tomrcroft <tomrcroft@yahoo.com>
2014-11-30 23:41:10 -0800

    integrated merge and added style to edit videos

--- addVideo.php
+++ addVideo.php
@@ -78,6 +78,7 @@
 				print("<h2><a href='./viewVideos.php'>View Videos!</a><h2>");
 				print("<h2><a href='./homepage.php'>Go Back Home!</a><h2>");
 				print("<h2><a href='./cleandata.php'>Clean the data</a><h2>");
+				
 			?>
 		</form>
 	</body>
