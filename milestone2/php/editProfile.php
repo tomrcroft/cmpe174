a145df2d3a4a1f37ac99b5b67f34ee87a1e3ffb5
johanhestenes <johanhestenes@users.noreply.github.com>
2014-12-13 21:36:36 -0800

    added navbar

--- milestone2/php/editProfile.php
+++ milestone2/php/editProfile.php
@@ -4,13 +4,20 @@
 	<link rel="stylesheet" type="text/css" href="../css/editProfileStyle.css">
 </head>
 <body>
+<ul>
+	<li><a href='./homepage.php'>Home</a></li>
+	<li><a href='./addVideo.php'>Add Video</a></li>
+	<li><a href='./viewVideos.php'>View Videos</a></li>
+	<li><a href='./editProfile.php'>Edit Profile</a></li>
+	<li><a href='./logout.php'>Logout</a></li>
+</ul>
+
 <h1>Edit Profile</h1>
 
 <p><a href="getEmail.php">Change Email</a></p>
 
 <p><a href="getPassword.php">Change Password</a></p>
 <br>
-<p><a href="homepage.php">Back to your homepage</a></p>
 
 </body>
 </html>
