0dccbd5af55a562d4d5a87617587a7782ad3941c
johanhestenes <johanhestenes@users.noreply.github.com>
2014-12-13 18:43:15 -0800

    Create editProfile.php

---
+++ milestone2/php/editProfile.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+	<title>Edit Profile</title>
+	<link rel="stylesheet" type="text/css" href="../css/editProfileStyle.css">
+</head>
+<body>
+<h1>Edit Profile</h1>
+
+<p><a href="getEmail.php">Change Email</a></p>
+
+<p><a href="getPassword.php">Change Password</a></p>
+<br>
+<p><a href="homepage.php">Back to your homepage</a></p>
+
+</body>
+</html>
