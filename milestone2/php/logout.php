c1af20054dd82d6b7a39be8de297a444da937ff1
johanhestenes <johanhestenes@users.noreply.github.com>
2014-12-13 18:51:59 -0800

    logout now goes to index.php

--- milestone2/php/logout.php
+++ milestone2/php/logout.php
@@ -4,16 +4,8 @@ unset($_SESSION);
 $_session= array();
 define('TITLE', 'Logout');
 session_destroy();
+
+header ("Location: index.php");
 ?>
 
-<html>
-	<head>
-		<title>Logout</title>
-		<link rel="stylesheet" type="text/css" href="../css/loginPageStyle.css">
-	</head>
-<body>
-	<h2> You are now logged out.</h2>
-	<h3><a href="index.php">Back to the login.</a></h3>
-</body>
 
-</html>
