fba14d871d07a375a973485fba92e158eb4afb7f
johanhestenes <johanhestenes@users.noreply.github.com>
2014-12-13 21:35:16 -0800

    added navbar

--- milestone2/php/getPassword.php
+++ milestone2/php/getPassword.php
@@ -4,6 +4,13 @@
 		  <link rel="stylesheet" type="text/css" href="../css/editProfileStyle.css">
     </head>
     <body>
+				<ul>
+					<li><a href='./homepage.php'>Home</a></li>
+					<li><a href='./addVideo.php'>Add Video</a></li>
+					<li><a href='./viewVideos.php'>View Videos</a></li>
+					<li><a href='./editProfile.php'>Edit Profile</a></li>
+					<li><a href='./logout.php'>Logout</a></li>
+				</ul>
 				<h1>Change Password</h1>
             <form action="changePassword.php" method="post">
 				<p>Old Password:
@@ -11,8 +18,6 @@
 				<p>New Password:
 				<input type="password" name="newpass"></p>
 				<p><input type="submit" name="submit" value="Submit"></p>
-				<br>
-				<p><a href="homepage.php">Back to your homepage</a></p>
             </form>
         </div>
     </body>
