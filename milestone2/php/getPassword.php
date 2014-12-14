7d171463b39a1f2fc22b600c6cb0440e44567b89
johanhestenes <johanhestenes@users.noreply.github.com>
2014-12-13 18:44:16 -0800

    Create getPassword.php

---
+++ milestone2/php/getPassword.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+        <title>Change Password</title>
+		  <link rel="stylesheet" type="text/css" href="../css/editProfileStyle.css">
+    </head>
+    <body>
+				<h1>Change Password</h1>
+            <form action="changePassword.php" method="post">
+				<p>Old Password:
+				<input type="password" name="oldpass"></p>
+				<p>New Password:
+				<input type="password" name="newpass"></p>
+				<p><input type="submit" name="submit" value="Submit"></p>
+				<br>
+				<p><a href="homepage.php">Back to your homepage</a></p>
+            </form>
+        </div>
+    </body>
+</html>
