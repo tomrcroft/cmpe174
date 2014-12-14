ab0ff5fc5155074948222f267ec331a0ef06bee9
johanhestenes <johanhestenes@users.noreply.github.com>
2014-12-13 18:44:34 -0800

    Create getEmail.php

---
+++ milestone2/php/getEmail.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+        <title>Change Email</title>
+		  <link rel="stylesheet" type="text/css" href="../css/editProfileStyle.css">
+    </head>
+    <body>
+			<h1>Change Email</h1>
+            <form action="changeEmail.php" method="post">
+				<p>New Email:
+				<input type="email" name="myusername"></p>
+				<p>Password:
+				<input type="password" name="pass"></p>
+				<p><input type="submit" name="submit" value="Submit"></p>
+				<br>
+				<p><a href="homepage.php">Back to your homepage</a></p>
+            </form>
+        </div>
+    </body>
+</html>
